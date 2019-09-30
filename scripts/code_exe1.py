1dfe3a0175fccfaaa8ad88ee247181fb3a70be7e
kulkarav <kulkarav@centos7.com>
2019-09-30 08:15:08 +0530

    learning python

---
+++ scripts/code_exe1.py
@@ -0,0 +1,6 @@
+# assign and print value using variables
+
+x = "3"
+y = ("Helllo, How are you ?")
+
+print(x,y)
---
+++ scripts/date_with_var.py
@@ -0,0 +1,3 @@
+import datetime
+mynew = datetime.datetime.now()
+print("The date and time is", mynew)
